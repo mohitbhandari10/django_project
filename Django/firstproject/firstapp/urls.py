41476d8948841bfb1ce0daa3afb5e7f12c516e50
mohitbhandari10 <isavitar10@gmail.com>
2025-08-29 01:16:11 +0530

    [ADD]upto-mysql/from-default-sqlite

--- Django/firstproject/firstapp/urls.py
+++ Django/firstproject/firstapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # Add your URL patterns here
     path('function', views.hello_world, name='hello_world'),
     path('class', views.HelloWorls.as_view(), name='hello_world_class'),
+    path('reservation', views.home, name='home'),
 ] 
--- Django/firstproject/firstapp/views.py
+++ Django/firstproject/firstapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm  # Import the ReservationForm
+
 
 # Create your views here.
 def hello_world(request):
@@ -11,3 +13,16 @@ class HelloWorls(View):
 
     def get(self,request):
         return HttpResponse("Heloo World class")
+
+
+def home(request):
+    
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Reservation created successfully!")
+        
+    return render(request, 'index.html', {'form': form})
